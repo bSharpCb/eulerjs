8d109ba4c6ccad20ab8ec03a256b0bc3fb1e0f92
Bradley Shaffer <bradsha0802@gmail.com>
2020-03-29 07:39:55 -0400

    Create problem2.js

---
+++ problem2.js
@@ -0,0 +1,12 @@
+function fibonacci(n){
+//where n represents the nth term of the fibonacci sequence
+    if(n<=1){
+        return 1
+    }
+    return fibonacci(n-1) + fibonacci(n-2)
+}
+
+let sum = 0;
+for(let i=1; fibonacci(i) < 4000000; i++){
+    fibonacci(i)%2==0 ? sum+= fibonacci(i) : sum = sum;
+}
